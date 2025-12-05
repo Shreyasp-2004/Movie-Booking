9a21b3bfdc799851c8cfa0f2f2529aa3812aeb01
Shreyas Palsodkar <221478802+Shreyasp-2004@users.noreply.github.com>
2025-12-05 13:32:32 +0530

    Update

--- src/main/java/com/shreyas/book_My_Show/controller/ShowController.java
+++ src/main/java/com/shreyas/book_My_Show/controller/ShowController.java
@@ -2,15 +2,16 @@ package com.shreyas.book_My_Show.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shreyas.book_My_Show.dto.ShowDTO;
+import com.shreyas.book_My_Show.service.MovieService;
 import com.shreyas.book_My_Show.service.ShowService;
 
 @RestController
@@ -23,8 +24,10 @@ public class ShowController {
         this.showService = showService;
     }
 
+    
+
     @PostMapping
-    public ResponseEntity<ShowDTO> addShow(@RequestBody ShowDTO showDTO) {
+    public ResponseEntity<ShowDTO> addShow(ShowDTO showDTO) {
         return ResponseEntity.ok(showService.addShow(showDTO));
     }
 
@@ -37,4 +40,9 @@ public class ShowController {
     public ResponseEntity<ShowDTO> getShowById(@PathVariable Long id) {
         return ResponseEntity.ok(showService.getShowById(id));
     }
+    @GetMapping("/movie/{movieId}")
+public ResponseEntity<List<ShowDTO>> getShowsByMovie(@PathVariable Long movieId) {
+    return ResponseEntity.ok(showService.getShowsByMovie(movieId));
+}
+
 }
